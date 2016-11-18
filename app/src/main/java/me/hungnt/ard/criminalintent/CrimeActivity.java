820aeb10f3963c84b86ca68e472493a61b73c983
Nguyen Thai Hung <nguyenthaihung@sutunam.com>
2016-11-18 17:22:15 +0700

    fragment on activity

--- app/src/main/java/me/hungnt/ard/criminalintent/CrimeActivity.java
+++ app/src/main/java/me/hungnt/ard/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package me.hungnt.ard.criminalintent;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,5 +11,12 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
--- app/src/main/java/me/hungnt/ard/criminalintent/CrimeFragment.java
+++ app/src/main/java/me/hungnt/ard/criminalintent/CrimeFragment.java
@@ -2,17 +2,17 @@ package me.hungnt.ard.criminalintent;
 
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
 import android.support.v4.app.Fragment;
-
-/**
- * Created by HungNT on 11/13/2016.
- */
+import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
+    private EditText mTitleField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +22,24 @@ public class CrimeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_crime, container, false);
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This one too
+            }
+        });
+
+        return v;
     }
 }
