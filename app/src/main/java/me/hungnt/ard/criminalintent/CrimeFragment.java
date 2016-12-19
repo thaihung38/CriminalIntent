cb0b6d2751db4d1cf820a118672e8a43873f9a70
Nguyen Thai Hung <nguyenthaihung@sutunam.com>
2016-12-19 13:37:26 +0700

    using Dialogs

--- app/src/main/java/me/hungnt/ard/criminalintent/CrimeFragment.java
+++ app/src/main/java/me/hungnt/ard/criminalintent/CrimeFragment.java
@@ -2,6 +2,7 @@ package me.hungnt.ard.criminalintent;
 
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -19,6 +20,7 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment
 {
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
 
     private Crime mCrime;
     private EditText mTitleField;
@@ -69,7 +71,14 @@ public class CrimeFragment extends Fragment
 
         mDateButton = (Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(DateFormat.format("EEE, MMM dd, yyyy hh:mm", mCrime.getDate()));
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
--- app/src/main/java/me/hungnt/ard/criminalintent/CrimeLab.java
+++ app/src/main/java/me/hungnt/ard/criminalintent/CrimeLab.java
@@ -18,7 +18,7 @@ public class CrimeLab {
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0); // Every other one
