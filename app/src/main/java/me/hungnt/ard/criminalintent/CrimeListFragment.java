5caa889393491c6697d6798b9c523f4acbe2f391
Nguyen Thai Hung <nguyenthaihung@sutunam.com>
2016-12-26 11:36:52 +0700

    menu layout

--- app/src/main/java/me/hungnt/ard/criminalintent/CrimeListFragment.java
+++ app/src/main/java/me/hungnt/ard/criminalintent/CrimeListFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -23,6 +25,14 @@ public class CrimeListFragment extends Fragment
     private CrimeAdapter mAdapter;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
 
@@ -38,6 +48,14 @@ public class CrimeListFragment extends Fragment
     {
         super.onResume();
         updateUI();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.fragment_crime_list, menu);
     }
 
     private void updateUI()
